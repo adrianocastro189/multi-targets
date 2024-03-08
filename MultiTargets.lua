ea28e0276f21d4312bba76e5cf2af7242a78e0b2
Adriano Castro <adrianostanley@gmail.com>
2024-03-08 16:56:58 -0300

    Tweaks to the addon main class

--- MultiTargets.lua
+++ MultiTargets.lua
@@ -1,26 +1,25 @@
 --[[
 Fires up the addon.
 ]]
-local function MultiTargets_initializeCore()
+function MultiTargets_initializeCore()
     -- initializes the addon data
     if not MultiTargets_Data then MultiTargets_Data = {} end
 
-    MultiTargets = {};
-    MultiTargets.__ = StormwindLibrary_v0_0_5.new()
+    MultiTargets = {}
+    MultiTargets.__ = StormwindLibrary_v0_0_6.new()
 end
 
 -- the main event frame used to trigger core initialization
-MultiTargetsInitializationFrame = CreateFrame('Frame');
+MultiTargetsInitializationFrame = CreateFrame('Frame')
 
 -- registers the PLAYER_LOGIN event
-MultiTargetsInitializationFrame:RegisterEvent('PLAYER_LOGIN');
+MultiTargetsInitializationFrame:RegisterEvent('PLAYER_LOGIN')
 
 -- fires up the MultiTargets addon when the player logs in
 MultiTargetsInitializationFrame:SetScript('OnEvent',
   function(self, event, ...)
-
     if event == 'PLAYER_LOGIN' then
-        MultiTargets_initializeCore();
+        MultiTargets_initializeCore()
     end
   end
-);
+)
