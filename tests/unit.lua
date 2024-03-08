2cf0e75cf9de16ebb4f8e0b4d293d5d445f7bb2f
Adriano Castro <adrianostanley@gmail.com>
2024-03-08 16:56:47 -0300

    Add the test suite

---
+++ tests/unit.lua
@@ -0,0 +1,22 @@
+-- mocks the CreateFrame World of Warcraft API method
+function CreateFrame(name)
+    local mockInstance = {}
+    mockInstance.__index = mockInstance
+    function mockInstance:RegisterEvent() end
+    function mockInstance:SetScript() end
+    return mockInstance
+end
+
+lu = require('luaunit')
+
+dofile('./lib/stormwind-library.lua')
+
+dofile('./MultiTargets.lua')
+
+dofile('src/Models/Target.lua')
+
+MultiTargets_initializeCore()
+
+lu.assertNotIsNil(MultiTargets)
+
+os.exit(lu.LuaUnit.run())
