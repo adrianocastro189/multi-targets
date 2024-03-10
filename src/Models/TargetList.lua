51b7c1ab40e7cb6bb04dd39450ecf26a9639f178
Adriano Castro <adrianostanley@gmail.com>
2024-03-10 17:19:28 -0300

    Add the TargetList:load() method

--- src/Models/TargetList.lua
+++ src/Models/TargetList.lua
@@ -10,7 +10,29 @@ TargetList.__index = TargetList
 Target list constructor.
 ]]
 function TargetList.__construct()
-    return setmetatable({}, TargetList)
+    local self = setmetatable({}, TargetList)
+
+    self.targets = {}
+
+    return self
+end
+
+--[[
+Loads a list of target instances by looking into the addon data.
+
+When called, this method will replace the inner targets property with the
+list of targets identified by the list name argument.
+
+@tparam string the target list name
+]]
+function TargetList:load(listName)
+    local arr = MultiTargets.__.arr
+
+    local targetList = arr:get(MultiTargets_Data, 'lists.' .. listName .. '.targets')
+
+    self.targets = arr:map(targetList, function (targetName)
+        return MultiTargets.__:new('MultiTargetsTarget', targetName)
+    end)
 end
 
 -- allows this class to be instantiated
--- tests/Models/TargetTestList.lua
+++ tests/Models/TargetTestList.lua
@@ -5,4 +5,31 @@ function testTargetListCanInstantiateTargetList()
     local targetList = MultiTargets.__:new('MultiTargetsTargetList')
 
     lu.assertNotIsNil(targetList)
+    lu.assertEquals(targetList.targets, {})
+end
+
+--[[
+@covers TargetList:load()
+]]
+function testTargetListCanLoad()
+    MultiTargets_Data = {}
+    MultiTargets.__.arr:set(MultiTargets_Data, 'lists.default.targets', {
+        'test-target-1',
+        'test-target-2',
+        'test-target-3',
+    })
+
+    local targetList = MultiTargets.__:new('MultiTargetsTargetList')
+
+    lu.assertEquals(#targetList.targets, 0)
+
+    targetList:load('default')
+
+    local targets = targetList.targets
+
+    -- @TODO: Compare list of targets
+    -- will left as failing to remind updating it
+    lu.assertEquals(#targetList.targets, 0)
+
+    MultiTargets_Data = nil
 end
