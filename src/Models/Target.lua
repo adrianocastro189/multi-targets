9400d12b3a3d646c8121a2fd8863a4197b6461c9
Adriano Castro <adrianostanley@gmail.com>
2024-03-14 15:44:25 -0300

    Add mark functions to Target

--- src/Models/Target.lua
+++ src/Models/Target.lua
@@ -4,58 +4,99 @@ The target model represents a target instance in a multi target context.
 Don't confuse this class with the Target facade in the Stormwind Library.
 This one is just a way to objectify a stored target.
 ]]
-
 local Target = {}
-Target.__index = Target
+    Target.__index = Target
 
---[[
-Target constructor.
-]]
-function Target.__construct(name)
-    local self = setmetatable({}, Target)
+    --[[
+    Target constructor.
+    ]]
+    function Target.__construct(name)
+        local self = setmetatable({}, Target)
 
-    self.name = name
+        self.name = name
+        self.markerIcon = 8 -- White Skull by default
 
-    return self
-end
+        return self
+    end
 
---[[
-Overrides the __eq() method to make sure == operator will be able to
-compare two target instances and determine if they're the same.
-]]
-function Target:__eq(target)
-    return target.name == self.name
-end
+    --[[
+    Overrides the __eq() method to make sure == operator will be able to
+    compare two target instances and determine if they're the same.
+    ]]
+    function Target:__eq(target)
+        return target.name == self.name
+    end
 
---[[
-Builds the macro body that will target the unit represented by the name
-stored in this instance.
-]]
-function Target:getMacroBody()
-    return {
-        '/cleartarget',
-        '/target [nodead] ' .. self.name,
-        '/script __h = UnitHealth("target") == UnitHealthMax("target")',
-        '/script if __h then SetRaidTarget("target",8) end',
-        '/run C_Timer.After(0.1, function() MultiTargets:rotate() end)',
-    }
-end
+    --[[
+    Builds the macro body that will target the unit represented by the name
+    stored in this instance.
+    ]]
+    function Target:getMacroBody()
+        return {
+            '/cleartarget',
+            '/target [nodead] ' .. self.name,
+            '/script __h = UnitHealth("target") == UnitHealthMax("target")',
+            '/script if __h then SetRaidTarget("target",8) end',
+            '/run C_Timer.After(0.1, function() MultiTargets:rotate() end)',
+        }
+    end
 
---[[
-Adds or update the main addon class, which is responsible for targeting one
-predefined target name and then rotate to the next one.
+    --[[
+    Determines whether this target instance is the current player target
+    in game.
 
-@codeCoverageIgnore won't have a unit test created for this one due to use
-                    only methods already tested and also due to mocking
-                    limitations at this point.
-]]
-function Target:updateMacro()
-    local macro = MultiTargets.__:new('Macro', 'MultiTargetsMacro')
+    @TODO: This method should be improved in the future to allow comparing
+           partial names, but this need more clarification of which parts
+           the /tar command will be able to match. <2024.03.14>
 
-    macro:setIcon('ability_hunter_focusedaim')
-    macro:setBody(self:getMacroBody())
-    macro:save()
-end
+    @treturn boolean
+    ]]
+    function Target:isTargetted()
+        return MultiTargets.__:getTarget():getName() == self.name
+    end
+
+    --[[
+    Marks the target using the icon set for this instance.
+
+    It's important to mention that this method will respect game's rules for
+    targeting, which means it won't mark a player from the opposite faction
+    for example.
+    ]]
+    function Target:mark()
+        MultiTargets.__:getTarget():mark(self.markerIcon)
+    end
+
+    --[[
+    Sets the marker icon for this target.
+
+    The list of available icons can be found at the Stormwind Library's
+    Target facade class.
+
+    @tparam int markerIcon
+
+    @treturn self
+    ]]
+    function Target:setMarkerIcon(markerIcon)
+        self.markerIcon = markerIcon
+        return self
+    end
+
+    --[[
+    Adds or update the main addon class, which is responsible for targeting one
+    predefined target name and then rotate to the next one.
+
+    @codeCoverageIgnore won't have a unit test created for this one due to use
+                        only methods already tested and also due to mocking
+                        limitations at this point.
+    ]]
+    function Target:updateMacro()
+        local macro = MultiTargets.__:new('Macro', 'MultiTargetsMacro')
+
+        macro:setIcon('ability_hunter_focusedaim')
+        macro:setBody(self:getMacroBody())
+        macro:save()
+    end
+-- end of Target
 
 -- allows this class to be instantiated
 MultiTargets.__:addClass('MultiTargetsTarget', Target)
--- tests/Models/TargetTest.lua
+++ tests/Models/TargetTest.lua
@@ -1,6 +1,6 @@
 TestTarget = {}
     -- @covers Target:__eq()
-    function TestTarget:testCanDetermineEquals()
+    function TestTarget:testEquals()
         local targetA = MultiTargets.__:new('MultiTargetsTarget', 'test-name')
         local targetB = MultiTargets.__:new('MultiTargetsTarget', 'test-another-name')
         local targetC = MultiTargets.__:new('MultiTargetsTarget', 'test-name')
@@ -13,7 +13,7 @@ TestTarget = {}
     end
 
     -- @covers Target:getMacroBody()
-    function TestTarget:testCanGetMacroBody()
+    function TestTarget:testGetMacroBody()
         local target = MultiTargets.__:new('MultiTargetsTarget', 'test-name')
 
         local macroBody = target:getMacroBody()
@@ -28,10 +28,42 @@ TestTarget = {}
     end
 
     -- @covers Target.__construct()
-    function TestTarget:testCanInstantiateTarget()
+    function TestTarget:testInstantiateTarget()
         local target = MultiTargets.__:new('MultiTargetsTarget', 'test-name')
 
         lu.assertNotIsNil(target)
         lu.assertEquals(target.name, 'test-name')
+        lu.assertEquals(target.markerIcon, 8)
+    end
+
+    -- @covers Target:isTargetted()
+    function TestTarget:testIsTargetted()
+        local function execution(targettedName, targetName, expectedResult)
+            local target = MultiTargets.__:new('MultiTargetsTarget', targetName)
+
+            local originalGetTarget = MultiTargets.__.getTarget
+
+            MultiTargets.__.getTarget = function ()
+                return {
+                    getName = function () return targettedName end
+                }
+            end
+
+            lu.assertEquals(target:isTargetted(), expectedResult)
+
+            MultiTargets.__.getTarget = originalGetTarget
+        end
+
+        execution('test-name', 'test-name', true)
+        execution('test-name', 'test-another-name', false)
+    end
+
+    -- @covers Target:setMarkerIcon()
+    function TestTarget:testSetMarkerIcon()
+        local target = MultiTargets.__:new('MultiTargetsTarget', 'test-name')
+
+        target:setMarkerIcon(1)
+
+        lu.assertEquals(target.markerIcon, 1)
     end
 -- end of TestTarget
