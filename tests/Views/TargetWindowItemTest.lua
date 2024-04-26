ab3c41ba0dbd9ded8432d4780982d7990a2f7664
Adriano Castro <adrianostanley@gmail.com>
2024-04-26 13:48:02 -0300

    Improve CreateFrame mocking

--- tests/Views/TargetWindowItemTest.lua
+++ tests/Views/TargetWindowItemTest.lua
@@ -20,4 +20,30 @@ TestTargetWindowItem = BaseTestClass:new()
         lu.assertIsTrue(instance.createFrameInvoked)
         lu.assertEquals(result, instance)
     end
+
+    -- @covers TargetWindowItem:createFrame()
+    function TestTargetWindowItem:testCreateFrame()
+        local instance = MultiTargets.__:new('MultiTargetsTargetWindowItem')
+
+        instance.createRaidMarker = function() instance.createRaidMarkerInvoked = true end
+        instance.createLabel = function() instance.createLabelInvoked = true end
+        instance.createRemoveButton = function() instance.createRemoveButtonInvoked = true end
+
+        local result = instance:createFrame()
+
+        lu.assertNotNil(instance.frame)
+
+        lu.assertEquals(result.backdrop, {
+            bgFile = 'Interface/Tooltips/UI-Tooltip-Background',
+            edgeFile = '',
+            edgeSize = 4,
+            insets = {left = 5, right = 1, top = 1, bottom = 1},
+        })
+        lu.assertEquals(result.backdropColor, {0, 0, 0, .2})
+        lu.assertEquals(result.height, 30)
+
+        lu.assertIsTrue(instance.createRaidMarkerInvoked)
+        lu.assertIsTrue(instance.createLabelInvoked)
+        lu.assertIsTrue(instance.createRemoveButtonInvoked)
+    end
 -- end of TestTargetWindowItem
